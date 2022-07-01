9e12f4877097a58d19e3f0c5a75fd48a08ae9a50
Faraz Ali Ahmad <farazali9028@gmail.com>
2022-07-01 23:12:04 +0530

    DSA Placement Day 1

--- pract.cpp
+++ pract.cpp
@@ -8,6 +8,12 @@ int main()
   array<int , 5> arr = {1,2,500,600,11};
   arr[1] = 5;
 
+
+  sort(arr.begin() , arr.end());
+
+  for(auto i:arr)
+    cout<<i<<endl;
+
   sort(arr.begin() , arr.end());
 
   for(auto i:arr)
